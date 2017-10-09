a66f70aa90de479a768ad127042b5f51d1b92d05
longtaoge <61852263@qq.com>
2017-10-09 21:44:41 +0800

    添加 静态页内容

--- README.md
+++ README.md
@@ -5,3 +5,17 @@ npm  龙套哥 命令行工具
 npm  命令行工具练习之作
 初学npm 的示例代码 
 
+
+
+
+### 安装
+npm install -g  longtaoge-cli
+
+longtaoge  --port 8080 --server ./www
+
+### 使用
+
+http://127.0.0.1:8080/index.html
+
+
+
--- longtaoge.js
+++ longtaoge.js
@@ -1,2 +1,63 @@
 #!/usr/bin/env node
-console.log('你好!longtaoge 命令行!');
+//console.log('你好!longtaoge 命令行!');
+
+
+const http =require('http');
+const url =require('url');
+const fs =require('fs');
+const path =require('path');
+const args = process.argv;
+
+console.log(args);
+
+
+//默认配置
+const  config={
+    PROT:8080,
+    basePath:process.cwd()
+}
+
+
+args.forEach((item,index)=>{
+    if(item==='--port'){
+
+        config.PORT=args[index+1];
+    }
+
+    if(item==='--server'){
+
+        config.basePath=args[index+1];
+    }
+
+
+})
+
+
+const  server= http.createServer((request,respose)=>{
+
+        const obj=url.parse(request.url);
+        console.log(config.basePath);
+        console.log(obj.pathname);
+          fs.readFile(path.join(config.basePath,obj.pathname),(err,data)=>{
+
+              if(err){
+                respose.writeHead(404);
+                return respose.end('404 not find the file');
+
+                }
+
+                respose.end(data);
+
+          });
+
+
+
+    })
+
+
+    server.listen(config.PORT,err=>{
+      if(err){
+      return console.log('监听出错了')
+    }
+        console.log('监听成功');
+    })
